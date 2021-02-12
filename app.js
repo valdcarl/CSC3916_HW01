348099bc902033def21922f4e84d704f45cbefdd
valdcarl <77943334+valdcarl@users.noreply.github.com>
2021-02-11 21:35:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -25,4 +25,4 @@ server.listen(process.env.PORT || 8008, () => {
 
 module.exports = server; // for testing
 
-curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
+curl -d "Hello World" -H "Content-Type: text" -X POST http://localhost:8008
